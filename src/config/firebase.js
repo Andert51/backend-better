7f033f3d1cf667e1573e1c575cf7875343e37c8e
Andert51 <andert@Nepthunie-VI.>
2024-10-05 18:55:34 -0600

    Back finished

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -7,7 +7,6 @@ const firebaseConfig = {
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     client_email: process.env.FIREBASE_CLIENT_EMAIL
-
 }
 
 admin.initializeApp({
---
+++ src/routes/employeeRoutes.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import authMiddleware from '../middleware/authMiddleware.js'
+import { check } from 'express-validator'
+import multer from 'multer'
+import {
+    createEmployee,
+    updateEmployee,
+    deleteEmployee,
+    getAllEmployee,
+    getEmployeeById,
+    getEmployeeByUsername,
+    getEmployeeByRol
+} from '../controllers/employeeController.js'
+
+const upload = multer ({ storage: multer.memoryStorage()})
+const router = express.Router()
+
+router.post(
+    '/create',
+    // middleware
+    upload.single('image'),
+    [
+        check('name').notEmpty().withMessage('Name is required'),
+        check('username').notEmpty().withMessage('Username is required'),
+        check('password').isLength({min: 6}).withMessage('Password must be at least 6 characters long'),
+    ],
+    createEmployee
+)
+
+router.put('/update/:id', authMiddleware, upload.single('image'), updateEmployee) 
+router.delete('/delete/:id', authMiddleware, deleteEmployee)
+router.get('/', authMiddleware, getAllEmployee)
+router.get('/employee/:id', authMiddleware, getEmployeeById)
+router.get('/rol/:rol', authMiddleware, getEmployeeByRol)
+router.get('/username/:username', authMiddleware, getEmployeeByUsername)
+
+export default router
