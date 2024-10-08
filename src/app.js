0303551c3f3aa7e056bc23f0df315ac8c37f5d4d
Andert51 <andrest05143@gmail.com>
2024-10-08 09:27:13 -0600

    Cors configured

--- src/app.js
+++ src/app.js
@@ -4,11 +4,19 @@ import errorHandler from './middleware/errorHandler.js'
 import rateLimitMiddleware from './middleware/rateLimit.js'
 import routes from './routes/index.js'
 
+import cors from 'cors'
+
+const corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+}
+
 dotenv.config()
 
 const app = express()
 app.use(express.json())
 app.use(rateLimitMiddleware)
+app.use(cors(corsOptions))
 app.use('/api/v1', routes) // El v1 es una practica de programacion para versionar las rutas
 app.use(errorHandler)
 
